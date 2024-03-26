49a1c71090da331ffed4ab5cb44239a4da38102a
isabelly buge <isabellyeduardabuge@gmail.com>
2024-03-26 09:13:25 -0300

    inicio dos comentarios

--- app/src/main/java/buge/isabelly/primeiraapp/MainActivity.java
+++ app/src/main/java/buge/isabelly/primeiraapp/MainActivity.java
@@ -8,16 +8,17 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {//a classe mainActivity e a activity principal da aplicacao
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {//onCreate e o metodo chamado toda vez que a tela esta sendo criada
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Button btnEnviar = findViewById(R.id.btnEnviar);//encontra o botão btnEnviar pelo id
-        btnEnviar.setOnClickListener(new View.OnClickListener() {
+        setContentView(R.layout.activity_main);//constroi/cria os elementos de interface que foram definidos no arquivo xml
+        Button btnEnviar = findViewById(R.id.btnEnviar);//obtem o botao btnEnviar atraves do id
+        btnEnviar.setOnClickListener(new View.OnClickListener() {//define o que vai acontecer quando o botao e clicado
+
             @Override
-            public void onClick(View v) {
+            public void onClick(View v) {//e executado quando o botao for clicado
                 EditText etDigiteAqui = findViewById(R.id.etDigiteAqui);//obtem o elemento de interface etDigiteAqui pelo id
                 String textoDigitado = etDigiteAqui.getText().toString();//pega o texto do etDigiteAqui e converte para string
                 Intent i = new Intent(MainActivity.this,NextActivity.class);//cria uma intenção, em que sinaliza a navegação de uma tela à outra
